8ef6dcbe1b436f6496d273d4b8c40ab131649fb9
Darrian <darriangaylor@icloud.com>
2021-05-03 06:22:17 -0400

    Update sv_config.lua

--- server/sv_config.lua
+++ server/sv_config.lua
@@ -1,14 +1,14 @@
 Config = {}
 
 Config.Locations = {
-    {coords = vector3(498.839, 5594.039, 795.480),       heading = 153.00, action = "tp1"},
-    {coords = vector3(1855.636, 2616.454, 45.672),      heading = 153.00, action = "tp2"},
-    {coords = vector3(869.567, -24.962, 78.764),        heading = 153.00, action = "tp3"},
-    {coords = vector3(427.150, -978.124, 30.709),       heading = 153.00, action = "tp4"},
-    {coords = vector3(794.535, 1279.255, 360.296),      heading = 153.00, action = "tp5"},
-    {coords = vector3(-1687.030, -1055.619, 13.091),    heading = 153.00, action = "tp6"},
-    {coords = vector3(-43.314, -784.259, 44.239),       heading = 153.00, action = "tp7"},
-    {coords = vector3(1723.865, 3256.002, 41.171),      heading = 153.00, action = "tp8"},
-    {coords = vector3(-1226.507, -2938.0312, 13.945),   heading = 153.00, action = "tp9"},
-    {coords = vector3(-2179.948, 3100.145, 32.810),     heading = 153.00, action = "tp10"}
+    {coords = vector3(499.161, 5604.105, 796.909),      heading = 161.66, action = "tp1"},
+    {coords = vector3(1855.636, 2616.454, 44.702),      heading = 186.06, action = "tp2"},
+    {coords = vector3(918.564, 50.426, 79.898),         heading = 238.16, action = "tp3"},
+    {coords = vector3(426.869, -981.809, 29.690),       heading = 268.86, action = "tp4"},
+    {coords = vector3(794.535, 1279.255, 359.296),      heading = 87.420, action = "tp5"},
+    {coords = vector3(-1687.030, -1055.619, 12.121),    heading = 153.00, action = "tp6"},
+    {coords = vector3(-43.314, -784.259, 43.239),       heading = 153.00, action = "tp7"},
+    {coords = vector3(1723.865, 3256.002, 40.171),      heading = 101.27, action = "tp8"},
+    {coords = vector3(-1002.705, -2959.879, 12.954),    heading = 66.470, action = "tp9"},
+    {coords = vector3(-2155.105, 3237.539, 31.810),     heading = 147.74, action = "tp10"}
 }
